4c1c5a1f4cd69bf0427e64646399716b57c418ed
aaa989 <1462467688@qq.com>
2026-05-19 14:52:59 +0800

    feat: 完成Day1环境搭建

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,17 +1,19 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import Detection from '../views/Detection.vue'
-
+import { createRouter, createWebHistory } from "vue-router";
+// import test_connect from "../views/test_connect.vue"; // 前后端连接测试页面
+import inference from "../views/Inference.vue"; // Yolo 推理验证页面
+// 路由配置
 const routes = [
   {
-    path: '/',
-    name: 'Detection',
-    component: Detection
+    path: "/",
+    name: "inference",
+    component: inference, // 默认打开就是检测页面
   }
-]
+];
 
+// 创建路由实例
 const router = createRouter({
   history: createWebHistory(),
-  routes
-})
+  routes,
+});
 
-export default router
+export default router;
